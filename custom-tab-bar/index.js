ad75e0003624ef8d80bc6c5fb7d4b64f3a6d37ca
zunzhuowei <1808862723@qq.com>
2019-08-02 16:02:51 +0800

    调节自定义tab bar 选中颜色

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,35 +1,35 @@
 Component({
-  data: {
-    selected: 0,
-    color: "#7A7E83",
-    selectedColor: "#3cc51f",
-    list: [{
-      pagePath: "/pages/index/index",
-      iconPath: "/images/index.png",
-      selectedIconPath: "/images/index_focus.png",
-      text: "主页"
-    }, {
-        pagePath: "/pages/index/index",
-      iconPath: "/images/discovery.png",
-      selectedIconPath: "/images/discovery_focus.png",
-      text: "发现"
-      }, {
-        pagePath: "/pages/index/index",
-        iconPath: "/images/chat.png",
-        selectedIconPath: "/images/chat_focus.png",
-        text: "私信"
-      }]
-  },
-  attached() {
-  },
-  methods: {
-    switchTab(e) {
-      const data = e.currentTarget.dataset
-      const url = data.path
-      wx.switchTab({url})
-      this.setData({
-        selected: data.index
-      })
+    data: {
+        selected: 0,
+        color: "#7A7E83",
+        selectedColor: "#0068C4",
+        list: [{
+            pagePath: "/pages/index/index",
+            iconPath: "/images/index.png",
+            selectedIconPath: "/images/index_focus.png",
+            text: "主页"
+        }, {
+            pagePath: "/pages/index/index",
+            iconPath: "/images/discovery.png",
+            selectedIconPath: "/images/discovery_focus.png",
+            text: "发现"
+        }, {
+            pagePath: "/pages/index/index",
+            iconPath: "/images/chat.png",
+            selectedIconPath: "/images/chat_focus.png",
+            text: "私信"
+        }]
+    },
+    attached() {
+    },
+    methods: {
+        switchTab(e) {
+            const data = e.currentTarget.dataset
+            const url = data.path
+            wx.switchTab({url})
+            this.setData({
+                selected: data.index
+            })
+        }
     }
-  }
 })
